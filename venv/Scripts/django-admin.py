72cdf3dc41947dc98cfb2dfed4df3900bfaf348a
Mauricio Rodriguez <mrodriguezlara@qualogy.com>
2019-04-15 17:09:52 +0200

    First Commit, adding base files

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mauricio Rodriguez\Documents\Personal\StarsWriters\WebPage_Test\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
